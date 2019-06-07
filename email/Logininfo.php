c622bfe44d071bcc1e11ea83144a9c697796b566
lingbohu <45029283+lingbohu@users.noreply.github.com>
2019-06-07 19:20:59 -0400

    Set up login information database table

---
+++ email/Logininfo.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Export to PHP Array plugin for PHPMyAdmin
+ * @version 0.2b
+ */
+
+/**
+ * Database `cse442_542_2019_summer_teamb_db`
+ */
+
+/* `cse442_542_2019_summer_teamb_db`.`Logininfo` */
+$Logininfo = array(
+  array('id' => '1','studentname' => 'Shikhar Chaure','emailaddress' => 'schaure@buffalo.edu','confirmationcode' => NULL,'coursrenumber' => 'CSE542'),
+  array('id' => '3','studentname' => 'Amanda Pellechia','emailaddress' => 'aepellec@buffalo.edu','confirmationcode' => NULL,'coursrenumber' => 'CSE542'),
+  array('id' => '2','studentname' => 'Lingbo Hu','emailaddress' => 'lingbohu@buffalo.edu','confirmationcode' => NULL,'coursrenumber' => 'CSE542')
+);
