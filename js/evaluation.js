0e9d84dbd819404efd319ba8e67cc6be99b87490
shikharchaure <shikharchaure@gmail.com>
2019-06-19 12:58:02 -0400

    Display saved scores onevaluation page #23

--- js/evaluation.js
+++ js/evaluation.js
@@ -39,10 +39,8 @@ function addSingleEvaluation(student, isSelf, index) {
 
   // create the role element
 var roleEval = "";
-if(student['evaluation']!=undefined)
-  roleEval = student['evaluation']['role'];
   var role = `Evaluation on  Role : <br>
-      <select class="form-control" id="role" name="role_` + index + `" required>
+      <select class="form-control" id="role` + index + `" name="role_` + index + `" required>
       <option>`+ roleEval +`</option>
 
       <option value=0> <strong> DOES NOT </strong> willingly assume team roles, <strong>RARELY </strong> completes assigned work</option>
@@ -53,28 +51,21 @@ if(student['evaluation']!=undefined)
   </div><br>`
   // add role to the page for the teammate
   $("#evaluation").append(role);
-  var options = document.getElementById("role");
-  for(i = 0; i < options.length; i++)
-  {
-    console.log(options[i]);
-      if(options[i].value == roleEval)
-      {
-          ret = options[i];
-          console.log(ret.text);
-          break;
+  //display evaluation if already filled once.
+    if(student['evaluation']!=undefined){
+      roleEval = student['evaluation']['role'];
+      $("#role"+index).val(roleEval);
       }
-  }
+
 
 
 
   var leadershipEval = "";
-  if(student['evaluation']!=undefined){
-    leadershipEval = student['evaluation']['leadership'];
-}
+
 
   // create the leadership element
   var leadership = `Evaluation on Leadership :
-    <select class="form-control" name="leadership_` + index + `" required>
+    <select class="form-control" id = "leadership_` + index + `" name="leadership_` + index + `" required>
       <option>`+ leadershipEval +`</option>
       <option value=0> <strong></strong>  <strong>RARELY </strong>takes leaderhip role; DOES not collaborate;<strong> SOMETIMES</strong> willing to assist teammates</option>
       <option value=1><strong></strong>   <strong>OCCASIONALLY</strong> shows leaderhip role, MOSTLY collaborate; <strong>GENERALLY</strong> willing to assist teammates</option>
@@ -87,13 +78,16 @@ if(student['evaluation']!=undefined)
 // add leadership to the page for the teammate
 $("#evaluation").append(leadership);
 
+if(student['evaluation']!=undefined){
+  leadershipEval = student['evaluation']['leadership'];
+  $("#leadership_"+index).val(leadershipEval);
+  }
 // create the participation element
 var participationEval = "";
-if(student['evaluation']!=undefined)
-  participationEval = student['evaluation']['participation'];
+
 
 var participation = `Evaluation on Participation :
-  <select class="form-control" name="participation_` + index + `" required ">
+  <select class="form-control" id = "participation_` + index + `" name="participation_` + index + `" required ">
     <option>`+ participationEval +`</option>
     <option value=0> <strong></strong> <strong>OFTEN</strong> misses meetings;Routinely unprepared for meetings, <strong>RARELY</strong> participates in meetings and does not share ideas.</option>
     <option value=1> <strong></strong> <strong> OCCASIONALLY</strong> misses or does not participate in meetings;<strong>SOMEWHAT</strong> prepared for meetings; Offers UNCLEAR or UNHELPFUL ideas in meetings.</option>
@@ -105,13 +99,18 @@ var participation = `Evaluation on Participation :
 
 // add the participation element to the page
 $("#evaluation").append(participation);
+//display evaluation if already filled once.
+if(student['evaluation']!=undefined){
+  participationEval = student['evaluation']['participation'];
+  $("#participation_"+index).val(participationEval);
+  }
+
+
 var professionalismEval = "";
-if(student['evaluation']!=undefined)
-professionalismEval = student['evaluation']['professionalism'];
 
 // create the professionalism element for the teammate
 var professionalism = `Evaluation on Professionalism :
-  <select class="form-control" name="professionalism_` + index + `" required id = "proff">
+  <select class="form-control" id = "professionalism_` + index + `" name="professionalism_` + index + `" required id = "proff">
     <option>`+ professionalismEval +`</option>
     <option value=0><strong></strong>  <strong>OFTEN</strong> discourteous and/or openly critical of teammates;  <strong> DOES NOT</strong> want to listen to any alternative perspective.</option>
     <option value=1><strong></strong>  <strong>NOT ALWAYS</strong> considerate or courteous towards teammates;  <strong>USUALLY</strong> appreciates teammates prespectives but often unwilling to consider them.</option>
@@ -123,12 +122,18 @@ var professionalism = `Evaluation on Professionalism :
 
 // add professionalism element to the page
 $("#evaluation").append(professionalism);
+if(student['evaluation']!=undefined){
+  professionalismEval = student['evaluation']['professionalism'];
+  $("#professionalism_"+index).val(professionalismEval);
+  }
+
+
+
 var qualityEval = "";
-if(student['evaluation']!=undefined)
-qualityEval = student['evaluation']['quality'];
+
 // create the quality element for the teammate
 var quality = `Evaluation on Quality :
-  <select class="form-control" name="quality_` + index + `" required>
+  <select class="form-control" id = "quality_` + index + `" name="quality_` + index + `" required>
     <option>`+ qualityEval +`</option>
     <option value=0>  <strong>RARELY</strong> commits to shared documents; Others  <strong>OFTEN </strong> required to revise, debug or fix their work.</option>
     <option value=1> <strong>OCCASIONALLY</strong> commits to shared documents; Others  <strong> SOMETIMES </strong>required to revise, debug or fix their work.</option>
@@ -141,6 +146,12 @@ var quality = `Evaluation on Quality :
 
 // append the quality element to the page
 $("#evaluation").append(quality);
+//display evaluation if already filled once.
+if(student['evaluation']!=undefined){
+  qualityEval = student['evaluation']['quality'];
+  $("#quality_"+index).val(qualityEval);
+  }
+
 // associate an index with a student id
 var studentId = `<input type="hidden" id="student_` + index + `" name="student_` + index + `"></input>`;
 $("#evaluation").append(studentId);
@@ -149,93 +160,3 @@ $("#evaluation").append(studentId);
 document.getElementById("student_" + index).value = student.studentId;
 
 }
-
-  function openLeadership() {
-    $(document).ready(function () {
-        $("#roleEval").click();
-    });
-  }
-  function openParticipation() {
-    $(document).ready(function () {
-        $("#participationEval").click();
-    });
-  }
-
-  function openProffesionalism() {
-    $(document).ready(function () {
-        $("#proffesionalEval").click();
-    });
-  } openQuality
-  function openQuality() {
-    $(document).ready(function () {
-        $("#qualityEval").click();
-    });
-  }
-var proffesionalEval = null;
-var roleEval = null;
-var leadershipEval = null;
-var participationEval = null;
-var qualityEval = null;
-  $(function(){
-      $(".role li a ").click(function(){
-
-        $(".btnRole:first-child").text($(this).text());
-        roleEval = $(".btnRole:first-child").val($(this).text());
-
-     });
-  });
-
-  $(function(){
-      $(".leaderhip li a ").click(function(){
-
-        $(".btnLeadership:first-child").text($(this).text());
-        leadershipEval= $(".btnLeadership:first-child").val($(this).text());
-
-     });
-  });
-
-  $(function(){
-      $(".participation li a ").click(function(){
-
-        $(".btnParticipation:first-child").text($(this).text());
-        participationEval= $(".btnParticipation:first-child").val($(this).text());
-
-     });
-  });
-
-   $(function(){
-      $(".professionalism li a ").click(function(){
-
-        $(".btnProfessionalism:first-child").text($(this).text());
-        proffesionalEval = $(".btnProfessionalism:first-child").val($(this).text());
-
-     });
-  });
-  $(function(){
-      $(".quality li a ").click(function(){
-
-        $(".btnQuality:first-child").text($(this).text());
-        qualityEval= $(".btnQuality:first-child").val($(this).text());
-
-     });
-  });
-  function sendEvaluation() {
-  var professionalism =  proffesionalEval.val();
-
-  //Dictionary to store all evals
-
-  var dict = {
-  RoleEval: roleEval.val(),
-  professionalEval: proffesionalEval.val(),
-  participationEval: participationEval.val(),
-  leadershipEval: leadershipEval.val(),
-  qualityEval : qualityEval.val()
-};
-  $.ajax({
-    type: "POST",
-    url: "/old/test.php",
-    data: {professionalism : professionalism}
-  }).then(function(result) {
-    window.location.href = "/old/testDropDown.html";
-  });
-  }
