e93f0e1a4c74044cf2e103cfcfd9f72b0530781e
lingbohu <alyhuus@gmail.com>
2019-07-01 13:58:23 -0400

    Create tables 'Courses' and 'StudentCourses' to store the student classes and handle class per confirmation code. #33

--- database/Courses.sql
+++ database/Courses.sql
@@ -1,12 +1,3 @@
--- phpMyAdmin SQL Dump
--- version 4.0.10.20
--- https://www.phpmyadmin.net
---
--- Host: tethys.cse.buffalo.edu:3306
--- Generation Time: Jun 18, 2019 at 01:30 PM
--- Server version: 5.1.65-log
--- PHP Version: 5.3.3
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
@@ -23,14 +14,24 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `Teams`
+-- Table structure for table `Courses`
 --
 
-CREATE TABLE IF NOT EXISTS `Teams` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `teamname` varchar(50) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=14 ;
+CREATE TABLE IF NOT EXISTS `Courses` (
+  `courseid` int(11) NOT NULL AUTO_INCREMENT,
+  `coursename` varchar(32) NOT NULL,
+  PRIMARY KEY (`courseid`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
+
+--
+-- Dumping data for table `Courses`
+--
+
+INSERT INTO `Courses` (`courseid`, `coursename`) VALUES
+(1, 'CSE 542'),
+(2, 'CSE 531'),
+(3, 'CSE 574'),
+(4, 'cse 573');
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
